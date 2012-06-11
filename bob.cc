5e3723b244c9ef266c8cb5b3dd56a6405fe22d3e
Max Neunhoeffer <neunhoef@mcs.st-and.ac.uk>
2012-06-11 22:47:35 +0100

    General cleanup of file structure.
    GPL, copyright notices.

--- bob.cc
+++ bob.cc
@@ -1,3 +1,10 @@
+//
+// bob.cc - part of the GAP installer BOB
+//
+// Copyright (C) by Max Neunhoeffer 2012
+// This file is free software, see license information at the end.
+//
+
 #include "bob.h"
 
 #include <stdio.h>
@@ -1572,4 +1579,19 @@ int main(int argc, char * const argv[], char *envp[])
         out(OK,"All went smoothly.");
     return 0;
 }
+
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
--- bob.h
+++ bob.h
@@ -1,4 +1,9 @@
-// bob.h - Copyright 2012 by Max Neunhoeffer
+//
+// bob.h - part of the GAP installer BOB
+//
+// Copyright (C) by Max Neunhoeffer 2012
+// This file is free software, see license information at the end.
+//
 
 #define BOBVERSION 9
 
@@ -153,4 +158,19 @@ void listdir(string dirname, vector<string> &names);
 // Throws ERROR if anything goes wrong.
 
 }  // namespace BOB
+
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
