73f321ac6046a18c9ba26e82c0cabcac39d8e515
Max Neunhoeffer <neunhoef@mcs.st-and.ac.uk>
2012-06-11 22:54:38 +0100

    New exclude.

--- scripts/cpsource.sh
+++ scripts/cpsource.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd .. 
-tar czvf bob.tar.gz bob --exclude .git  --exclude autom4te.cache --exclude TODO
+tar czvf bob.tar.gz bob --exclude .git  --exclude autom4te.cache --exclude TODO --exclude .deps
 scp bob.tar.gz neunhoef@schur.mcs.st-and.ac.uk:/scratch/neunhoef/mywebpage.pub/Computer/Software/Gap/bob/bob.tar.gz
