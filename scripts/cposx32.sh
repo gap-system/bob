6795ceab0780d4aaae9f95dbf6cb7cf5e9227701
Max Neunhoeffer <neunhoef@mcs.st-and.ac.uk>
2012-06-14 10:59:49 +0100

    Also make .tar.gz for Mac OS X.

--- scripts/cposx32.sh
+++ scripts/cposx32.sh
@@ -6,3 +6,6 @@ cp -a bob GAP_Installer_BOB
 cp -a BOB.app GAP_Installer_BOB
 hdiutil create -srcfolder GAP_Installer_BOB bob-osx.dmg
 scp bob-osx.dmg neunhoef@schur.mcs.st-and.ac.uk:/scratch/neunhoef/mywebpage.pub/Computer/Software/Gap/bob/bob-osx.dmg
+cd GAP_Installer_BOB
+tar czvf ../bob-osx.tar.gz bob README BOB.app
+scp bob-osx.tar.gz neunhoef@schur.mcs.st-and.ac.uk:/scratch/neunhoef/mywebpage.pub/Computer/Software/Gap/bob/bob-osx.tar.gz
