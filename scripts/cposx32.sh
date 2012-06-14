1c1c8c4148d7d99f8f8c8c413163081ece674c89
Max Neunhoeffer <neunhoef@mcs.st-and.ac.uk>
2012-06-14 11:01:15 +0100

    Fix new script.

--- scripts/cposx32.sh
+++ scripts/cposx32.sh
@@ -8,4 +8,4 @@ hdiutil create -srcfolder GAP_Installer_BOB bob-osx.dmg
 scp bob-osx.dmg neunhoef@schur.mcs.st-and.ac.uk:/scratch/neunhoef/mywebpage.pub/Computer/Software/Gap/bob/bob-osx.dmg
 cd GAP_Installer_BOB
 tar czvf ../bob-osx.tar.gz bob README BOB.app
-scp bob-osx.tar.gz neunhoef@schur.mcs.st-and.ac.uk:/scratch/neunhoef/mywebpage.pub/Computer/Software/Gap/bob/bob-osx.tar.gz
+scp ../bob-osx.tar.gz neunhoef@schur.mcs.st-and.ac.uk:/scratch/neunhoef/mywebpage.pub/Computer/Software/Gap/bob/bob-osx.tar.gz
